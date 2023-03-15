a28fdf2643ef862c8f54561bc64442b8ad3950b0
yuhsintian <sarah711041@gmail.com>
2023-03-15 16:17:02 +0800

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShareModule } from './share/share.module';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
+
+
+
+@NgModule({
+  declarations:[
+    AppComponent,
+    ToolbarComponent
+  ],
+
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ShareModule,
+  ],
+schemas:[CUSTOM_ELEMENTS_SCHEMA],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/share/share.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PrimengModule } from './primeng/primeng.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    PrimengModule,
+  ]
+})
+export class ShareModule { }
